b1a07576a7cde4d2be58c40cce18987632a5fa4b
Guitchar Live <marcosfullstack@gmail.com>
2020-03-01 13:02:00 -0300

    estudo sobre EcmaScript6

--- bundle.js
+++ bundle.js
@@ -7,7 +7,7 @@ var titleSection = function titleSection(title, text) {
 titleSection('operaçõs em arrays =>', 'utilizando função map()');
 /*Função array.map() */
 
-var arr = [1, 2, 3, 4, 5];
+var arr = [1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 titleSection('', 'utilizando função map() em sua forma original');
 console.log("array principal", arr);
 var newArraWithMap = arr.map(function (item) {
@@ -51,4 +51,49 @@ function divi(item) {
 function sub(item) {
   return item - 2;
 }
+
+console.log("");
 /*função  reducer */
+
+console.log("");
+titleSection('', 'utilizando função reducer() em sua forma original');
+console.log("array principal", arr);
+var newArraWithReduce = arr.reduce(function (accumulator, itemOfArray, indexOfArray, array) {
+  return accumulator + itemOfArray; // rodando map e multiplicando o valor dos items por ele mesmo
+});
+console.log("");
+titleSection('', 'utilizando função filter() em sua forma original');
+console.log("array principal", arr);
+var newArraWithFilter = arr.filter(function (item) {
+  return item >= 10 && item <= 15;
+});
+var namePersons = ["Joana", 'Paulo', 'José', 'Ana', 'Aline', 'Marcos'];
+
+function filterName(name) {
+  return namePersons.filter(function (item) {
+    return item === name;
+  });
+}
+
+function findPerson(name) {
+  var person = {
+    name: "",
+    notFound: ""
+  };
+
+  if (filterName(name).length > 0) {
+    person.name = filterName(name)[0];
+    person.notFound = false;
+  } else {
+    person.name = "nada encontrado";
+    person.notFound = true;
+  }
+
+  return person;
+}
+
+var marcos = findPerson("Marddcos");
+
+if (marcos.notFound) {
+  console.log(marcos.notFound);
+} else [console.log(marcos.name)];
--- main.js
+++ main.js
@@ -4,7 +4,7 @@ titleSection('operaçõs em arrays =>','utilizando função map()');
 
 /*Função array.map() */
 
-const arr = [1,2,3,4,5];
+const arr = [1,2,3,4,6,7,8,9,10,11,12,13,14,15,16];
 
 titleSection('','utilizando função map() em sua forma original');
 console.log("array principal",arr);
@@ -31,6 +31,7 @@ function calc(array,op = soma){
 }
 
 function soma(item){
+
     return item+2;
 }
 
@@ -45,6 +46,46 @@ function divi(item){
 function sub(item){
     return item-2;
 }
-
+console.log("");
 /*função  reducer */
+console.log("");
+titleSection('','utilizando função reducer() em sua forma original');
+console.log("array principal",arr);
+const newArraWithReduce = arr.reduce((accumulator,itemOfArray,indexOfArray,array)=>{
+    return accumulator+itemOfArray;// rodando map e multiplicando o valor dos items por ele mesmo
+}); 
+console.log("");
+titleSection('','utilizando função filter() em sua forma original');
+console.log("array principal",arr);
+const newArraWithFilter = arr.filter((item)=>{
+   
+    return item >= 10  && item <= 15
+    
+}); 
 
+const namePersons=["Joana",'Paulo','José','Ana','Aline','Marcos'];
+
+function filterName(name){
+   return namePersons.filter((item)=>{
+        return item === name 
+    }); 
+}
+
+function findPerson(name){
+   let person={name:"",notFound:""} ;
+    if (filterName(name).length > 0){
+        person.name = filterName(name)[0];
+        person.notFound=false;
+    }else{
+        person.name = "nada encontrado";
+        person.notFound=true;
+    }
+    return person;
+}
+let marcos = findPerson("Marddcos");
+
+if(marcos.notFound){
+    console.log(marcos.notFound)
+}else[
+    console.log(marcos.name)
+]
